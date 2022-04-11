8370f8e3a452dafd8b86cf32ee35cd1525d7122c
yohannavrojo <yohavale7883@gmail.com>
2022-04-11 16:51:58 -0300

    cambios para producto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-// import logo from './logo.svg';
 // import Cartas from './components/Cartas';
 import Producto from './components/CartasProducto/Producto'
 // import Prueba from "./components/Prueba";
--- src/components/CartasProducto/Producto.js
+++ src/components/CartasProducto/Producto.js
@@ -4,8 +4,9 @@ import imagen from "../imagenes/fondocarta.jpg";
 
 function Producto() {
   return (
-
-
+<>
+<div className="contenedor ">
+<div className="carta-container">
       <div className="carta">
         <div className="carta-header">
           <img src={imagen} />
@@ -22,11 +23,109 @@ function Producto() {
           <i className="fas fa-comment"></i>
           <i className="fas fa-bookmark"></i>
         </div>
-        {/* ------------------ */}
+    </div>   
        
       </div>
-   
+
+      <div className="carta-container">
+      <div className="carta">
+        <div className="carta-header">
+          <img src={imagen} />
+          <div>
+            <p> Nombre</p>
+            <p>@Nombre</p>
+          </div>
+        </div>
+        <div className="carta-body">
+          <img src={imagen} />
+        </div>
+        <div className="carta-footer">
+          <i className="fas fa-heart"></i>
+          <i className="fas fa-comment"></i>
+          <i className="fas fa-bookmark"></i>
+        </div>
+    </div>   
+       
+      </div>
+
+</div>
+
+
+
+
+     
+     {/* <ul className="cards">
+  <li>
+    <a href="" className="card">
+      <img src={imagen}className="card__image" alt="" />
+      <div className="card__overlay">
+        <div className="card__header">
+          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+          <img className="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+          <div className="card__header-text">
+            <h3 className="card__title">Jessica Parker</h3>            
+            <span className="card__status">1 hour ago</span>
+          </div>
+        </div>
+        <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+      </div>
+    </a>      
+  </li>
+  <li>
+    <a href="" className="card">
+      <img src="https://i.imgur.com/2DhmtJ4.jpg" className="card__image" alt="" />
+      <div className="card__overlay">        
+        <div className="card__header">
+          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+          <img className="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
+          <div className="card__header-text">
+            <h3 className="card__title">kim Cattrall</h3>
+            <span className="card__status">3 hours ago</span>
+          </div>
+        </div>
+        <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+      </div>
+    </a>
+  </li>
+  <li>
+    <a href="" className="card">
+      <img src="https://i.imgur.com/oYiTqum.jpg" className="card__image" alt="" />
+      <div className="card__overlay">
+        <div className="card__header">
+          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+          <img className="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+          <div className="card__header-text">
+            <h3 className="card__title">Jessica Parker</h3>
+            <span className="card__tagline">Lorem ipsum dolor sit amet consectetur</span>            
+            <span className="card__status">1 hour ago</span>
+          </div>
+        </div>
+        <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+      </div>
+    </a>
+  </li>
+  <li>
+    <a href="" className="card">
+      <img src="https://i.imgur.com/2DhmtJ4.jpg" className="card__image" alt="" />
+      <div className="card__overlay">
+        <div className="card__header">
+          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+          <img className="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
+          <div className="card__header-text">
+            <h3 className="card__title">kim Cattrall</h3>
+            <span className="card__status">3 hours ago</span>
+          </div>          
+        </div>
+        <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+      </div>
+    </a>
+  </li>    
+</ul> */}
+       
+
+     
     
+    </>
   );
 }
 
