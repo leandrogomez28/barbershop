08fd28bc6d0352fdb66200b8111e4372d228dad1
leandrogomez28 <leandro.gomezdxn@gmail.com>
2022-04-12 20:25:45 -0300

    armando

--- src/App.js
+++ src/App.js
@@ -9,28 +9,9 @@ import SignUp from './components/SignUp';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       {/* <Titulo/>
       <Carrousel/> */}
       <SignUp/>
-=======
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-      <Gallery/>
->>>>>>> 06847e74a224bbcadb429ce25bebdee324cd5c53
     </div>
   );
 }
