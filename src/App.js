f3a646d6c8cdcfa70373b8f986c013428714c05a
jeanney11 <jclayrz11@gmail.com>
2022-04-12 21:35:58 -0300

    Cards Barberos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import Estilistas from './components/Estilistas.js'
+// import Estilyst from './components/Estilyst.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Estilistas/>
+      {/* <Estilyst/> */}
     </div>
   );
 }
---
+++ src/components/Estilistas.js
@@ -0,0 +1,142 @@
+import React from "react";
+import "./barberos.css"
+import facebook from '../components/Imagen1/facebook.png'
+import twitter from '../components/Imagen1/twitter.png'
+import whatsapp from '../components/Imagen1/whatsapp.png'
+import instagram from '../components/Imagen1/instagram.png'
+
+
+
+function Estilista(){
+
+    return(
+
+<div className="barber">
+
+<aside class="profile-card">
+
+  <header>
+
+    {/* <!-- here’s the avatar --> */}
+    <a href="https://tutsplus.com">
+      <img src="https://randomuser.me/api/portraits/men/99.jpg"/>
+    </a>
+
+    {/* <!-- the username --> */}
+    <h1>George Darkos</h1>
+
+    {/* <!-- and role or location --> */}
+    <h2>- Full Stack Web Developer -</h2>
+
+  </header>
+
+  {/* <!-- bit of a bio; who are you? --> */}
+  <div class="profile-bio">
+
+    <p>Hello there!</p>
+    <p>I am a full stack web developer. I mainly work with PHP, HTML, CSS, JS and WordPress.
+      <br />I also play well with Photoshop, Corel Draw, After Effects and other cool stuff.</p>
+
+  </div>
+
+  {/* <!-- some social links to show off --> */}
+  <ul class="profile-social-links">
+
+    {/* <!-- twitter - el clásico  --> */}
+    <li>
+      <a href="https://twitter.com/tutsplus">
+        <img src={twitter}/>
+      </a>
+    </li>
+
+    {/* <!-- envato – use this one to link to your marketplace profile --> */}
+    <li>
+      <a href="https://envato.com">
+        <img src={instagram}/>
+      </a>
+    </li>
+
+    {/* <!-- codepen - your codepen profile--> */}
+    <li>
+      <a href="https://codepen.io/tutsplus">
+        <img src={whatsapp}/>
+      </a>
+    </li>
+
+    {/* <!-- add or remove social profiles as you see fit --> */}
+    <li>
+      <a href="https://www.facebook.com/">
+        <img src={facebook}/>
+      </a>
+    </li>
+  </ul>
+</aside>
+
+<aside class="profile-card">
+
+  <header>
+
+    {/* <!-- here’s the avatar --> */}
+    <a href="https://tutsplus.com">
+      <img src="https://randomuser.me/api/portraits/men/99.jpg"/>
+    </a>
+
+    {/* <!-- the username --> */}
+    <h1>George Darkos</h1>
+
+    {/* <!-- and role or location --> */}
+    <h2>- Full Stack Web Developer -</h2>
+
+  </header>
+
+  {/* <!-- bit of a bio; who are you? --> */}
+  <div class="profile-bio">
+
+    <p>Hello there!</p>
+    <p>I am a full stack web developer. I mainly work with PHP, HTML, CSS, JS and WordPress.
+      <br />I also play well with Photoshop, Corel Draw, After Effects and other cool stuff.</p>
+
+  </div>
+
+  {/* <!-- some social links to show off --> */}
+  <ul class="profile-social-links">
+
+    {/* <!-- twitter - el clásico  --> */}
+    <li>
+      <a href="https://twitter.com/tutsplus">
+        <img src={twitter}/>
+      </a>
+    </li>
+
+    {/* <!-- envato – use this one to link to your marketplace profile --> */}
+    <li>
+      <a href="https://envato.com">
+        <img src={instagram}/>
+      </a>
+    </li>
+
+    {/* <!-- codepen - your codepen profile--> */}
+    <li>
+      <a href="https://codepen.io/tutsplus">
+        <img src={whatsapp}/>
+      </a>
+    </li>
+
+    {/* <!-- add or remove social profiles as you see fit --> */}
+    <li>
+      <a href="https://codepen.io/tutsplus">
+        <img src={facebook}/>
+      </a>
+    </li>
+  </ul>
+</aside>
+
+
+
+        </div>
+
+    );
+
+}
+
+export default Estilista;
