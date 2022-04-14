4cc02c6dcb6fad3afb6e3d47f9b1978b7d4112cf
leandrogomez28 <leandro.gomezdxn@gmail.com>
2022-04-14 19:49:24 -0300

    new changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Carrousel from './components/Carrousel'
 import Titulo from './components/Titulo';
 import SignUp from './components/SignUp';
 
-import logo from './logo.svg';
+
 import Estilistas from './components/Estilistas.js'
 // import Estilyst from './components/Estilyst.js'   esta no se va a usar
 // import ValesGift from './components/ValesGift.js'
