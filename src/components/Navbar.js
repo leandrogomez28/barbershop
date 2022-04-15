4c3916560fc15254f4768a764cc5ec8e98334a05
kairayary <kaira.abreu@gmail.com>
2022-04-15 15:03:37 -0300

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+
+import React from 'react';
+import "./Navbar.css";
+
+
+
+function Navbar() {
+    return (
+        <>
+
+
+            <nav>
+                <ul>
+                    <li>
+                        <div className="home-icon bg-dark opacity-75">
+                            <div className="roof">
+                                <div className="roof-edge"></div>
+                            </div>
+                            <div className="front"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="about-icon">
+                            <div className="head">
+                                <div className="eyes"></div>
+                                <div className="beard"></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="work-icon">
+                            <div className="paper"></div>
+                            <div className="lines"></div>
+                            <div className="lines"></div>
+                            <div className="lines"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="mail-icon">
+                            <div className="mail-base">
+                                <div className="mail-top"></div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+
+
+
+
+
+        </>
+    )
+};
+
+export default Navbar;
