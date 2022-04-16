b36d80fd702d92b9b7472351ae17915c484ebcae
yohannavrojo <yohavale7883@gmail.com>
2022-04-16 14:36:13 -0300

    cambios estilo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react"
 import Cartas from './components/Cartas';
+import Cariusel2 from "./components/CartasProducto/Cariusel2";
 import Producto from './components/CartasProducto/Producto'
+import Mostrar from "./components/mostrar/Mostrarcards";
 
 function App() {
   return (
-    
-      // <Cartas/>
-      <Producto/>
-
+    <div>
+      {/* <Cartas/>
+      <Producto/> */}
+{/* <Mostrar/> */}
+<Cariusel2/>
+</div>
   );
 }
 
