fa05b61605c5cd5627e480ffd7bf7a597bf89544
jeanney11 <jclayrz11@gmail.com>
2022-04-18 14:50:54 -0300

    actualizacion estilos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
-import Estilistas from './components/Estilistas.js'
-// import Estilyst from './components/Estilyst.js'   esta no se va a usar
-// import ValesGift from './components/ValesGift.js'
+//  import Estilistas from './components/Estilistas.js'
+import Estilyst from './components/Estilyst.js'   //esta no se va a usar
+//  import ValesGift from './components/ValesGift.js'
 
 
 function App() {
   return (
     <div className="App">
-      <Estilistas/>
-      {/* <Estilyst/> Es no se va a usar */}
+      {/* <Estilistas/> */}
+      <Estilyst/>    {/* Es no se va a usar */}
       {/* <ValesGift/> */}
     </div>
   );
