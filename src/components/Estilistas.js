8787f30dc3d037bf29ae475947bbdccea991d276
jeanney11 <jclayrz11@gmail.com>
2022-04-19 15:09:14 -0300

    Modificacion de Barberos y base de datos

--- src/components/Estilistas.js
+++ src/components/Estilistas.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import "./barberos.css";
-import facebook from "../components/Imagen1/facebook.png";
-import twitter from "../components/Imagen1/twitter.png";
-import whatsapp from "../components/Imagen1/whatsapp.png";
-import instagram from "../components/Imagen1/instagram.png";
-import LikesBarber from "./LikesBarber";
-=======
 import React, { useEffect } from "react";
 import "./barberos.css"
 import facebook from '../components/Imagen1/facebook.png'
@@ -16,26 +7,8 @@ import instagram from '../components/Imagen1/instagram.png'
 import { actionType } from '../reducer';
 import { useStateValue } from '../StateProvider';
 import axios from 'axios';
->>>>>>> ae9832a23c78165c83e6acffac2b44a795540309
 
-function Estilista() {
-  return (
-    <div className="barber">
-      <aside class="profile-card">
-        <header>
-          {/* <!-- here’s the avatar --> */}
-          <a href="https://tutsplus.com">
-            <img src="https://randomuser.me/api/portraits/men/99.jpg" />
-          </a>
 
-          {/* <!-- the username --> */}
-          <h1>Eder Aponte</h1>
-
-<<<<<<< HEAD
-          {/* <!-- and role or location --> */}
-          <h2>- Barber Masters -</h2>
-        </header>
-=======
 function Estilista() {
   const [{ barberos }, dispatch] = useStateValue()
 
@@ -50,36 +23,9 @@ function Estilista() {
       })
   }, [])
 
->>>>>>> ae9832a23c78165c83e6acffac2b44a795540309
 
-        {/* <!-- bit of a bio; who are you? --> */}
-        <div class="profile-bio">
-          
-        <div>
-            <a class="bn39" href="/">
-              <span class="bn39span">Button</span>
-            </a>
-          </div>
+       
 
-<<<<<<< HEAD
-          <p>
-            Emprendedor y visionario con vasta experiencia en el ramo de la
-            barbería, con una audaz y llamativa propuesta en Buenos Aires de un
-            estilo neoclásico que resalta su gran maestría y versatilidad en
-            tijera y navajas para trabajar el cabello y la barba.
-          </p>
-        </div>
-
-        {/* <!-- some social links to show off --> */}
-        <ul class="profile-social-links">
-          {/* <!-- twitter - el clásico  --> */}
-          <li>
-            <a href="https://twitter.com/tutsplus">
-              <img src={twitter} />
-            </a>
-           
-          </li>
-=======
   return (
 
     <div className="barber">
@@ -92,24 +38,25 @@ function Estilista() {
 
               {/* <!-- here’s the avatar --> */}
               <a href="https://tutsplus.com">
-                <img src="https://randomuser.me/api/portraits/men/99.jpg" />
+              <img src={process.env.PUBLIC_URL +`/barberosGaleria/${item.img}`}/>
               </a>
 
               {/* <!-- the username --> */}
-              <h1>{item.nombre}</h1>
+              <h1>{item.nombre} {item.apellido}</h1>
 
               {/* <!-- and role or location --> */}
-              <h2>- Barber Masters -</h2>
+              <h2>- {item.especialidad} -</h2>
 
             </header>
 
             {/* <!-- bit of a bio; who are you? --> */}
             <div class="profile-bio">
-
-              <p>Hello there!</p>
-              <p>Emprendedor y visionario con vasta experiencia en el ramo de la barbería, con una audaz y llamativa propuesta en Buenos Aires
-                de un estilo neoclásico que resalta su gran maestría y versatilidad en tijera y navajas para trabajar el cabello y la barba.</p>
-
+              <div className="turno">
+            <a class="bn39" href="/"><span class="bn39span">Turno</span></a>
+            </div>
+            <div className="resumen">
+              <p>{item.reseña}</p>
+              </div>
             </div>
 
             {/* <!-- some social links to show off --> */}
@@ -152,93 +99,6 @@ function Estilista() {
 
     </div>
 
-  );
->>>>>>> ae9832a23c78165c83e6acffac2b44a795540309
-
-          {/* <!-- envato – use this one to link to your marketplace profile --> */}
-          <li>
-            <a href="https://envato.com">
-              <img src={instagram} />
-            </a>
-          </li>
-
-          {/* <!-- codepen - your codepen profile--> */}
-          <li>
-            <a href="https://codepen.io/tutsplus">
-              <img src={whatsapp} />
-            </a>
-          </li>
-
-          {/* <!-- add or remove social profiles as you see fit --> */}
-          <li>
-            <a href="https://www.facebook.com/">
-              <img src={facebook} />
-            </a>
-          </li>
-        </ul>
-      </aside>
-
-      <aside class="profile-card">
-        <header>
-          {/* <!-- here’s the avatar --> */}
-          <a href="https://tutsplus.com">
-            <img src="https://randomuser.me/api/portraits/men/99.jpg" />
-          </a>
-
-          {/* <!-- the username --> */}
-          <h1>Tomas Martinez</h1>
-
-          {/* <!-- and role or location --> */}
-          <h2>- Barber Pro -</h2>
-        </header>
-
-        {/* <!-- bit of a bio; who are you? --> */}
-        <div class="profile-bio">
-          <div>
-            <a class="bn39" href="/">
-              <span class="bn39span">Button</span>
-            </a>
-          </div>
-          <p>
-            Amplia formación y experiencia es reconocido como uno de los mejores
-            y más famosos referentes en Caracas y ahora en Buenos Aires. Es
-            especialista en cortes urbanos y siempre está a la vanguardia con
-            los estilos más trendy.
-          </p>
-        </div>
-
-        {/* <!-- some social links to show off --> */}
-        <ul class="profile-social-links">
-          {/* <!-- twitter - el clásico  --> */}
-          <li>
-            <a href="https://twitter.com/tutsplus">
-              <img src={twitter} />
-            </a>
-          </li>
-
-          {/* <!-- envato – use this one to link to your marketplace profile --> */}
-          <li>
-            <a href="https://envato.com">
-              <img src={instagram} />
-            </a>
-          </li>
-
-          {/* <!-- codepen - your codepen profile--> */}
-          <li>
-            <a href="https://codepen.io/tutsplus">
-              <img src={whatsapp} />
-            </a>
-          </li>
-
-          {/* <!-- add or remove social profiles as you see fit --> */}
-          <li>
-            <a href="https://codepen.io/tutsplus">
-              <img src={facebook} />
-            </a>
-          </li>
-        </ul>
-      </aside>
-    </div>
   );
 }
 
