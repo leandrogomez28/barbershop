2504e4a59352eaf24632d011414cd5526a6b05e3
yohannavrojo <yohavale7883@gmail.com>
2022-04-19 16:04:37 -0300

    mostrar cartas

--- src/components/mostrar/Mostrarcards.js
+++ src/components/mostrar/Mostrarcards.js
@@ -1,46 +1,54 @@
-import React from 'react'
-import "./Mostrarcards.css"
-import imagenprueba from "../imagenes/productos general/activador.png"
-// import Ayudar from './Ayudar';
+import React from "react";
+import "./Mostrarcards.css";
+import imagenprueba from "../imagenes/productos general/activador.png";
 
-
-function Mostrarcards () {
+import { GiMustache } from "react-icons/gi";
+function Mostrarcards() {
   return (
-    <>
-
-<div className='contieneCartas'> 
-<div className='img-cartas'>
-<img src={imagenprueba}/>
-</div>
-  <div>
-<h1 className='titulo'> hola </h1>
- <h4 className='subtitulo' >subtitulo </h4>
- <div class="control">
-	
-  {/* <!-- Start Button buying --> */}
-    <button class="btn">
-  {/* <!-- 		the Price --> */}
-     {/* <span class="price">49 $</span> */}
-  {/* <!-- 		shopping cart icon--> */}
-     {/* <span class="shopping-cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span> */}
-  {/* <!-- 		Buy Now / ADD to Cart--> */}
-     <span class="buy">Buy Now</span>
-   </button>
-    {/* <!-- End Button buying --> */}
     
-  </div> 
- 
-  </div>
+    <div className="fondomostrar" >
+      {/* galeria */}
+      <div className="div"></div>
+      {/* titulo */}
+      <div className="encabezado">
+        <h1 className="titulo">MEN’S PRODUCTS </h1>
+        <h4 className="subtitulo1">
+          Evolucionar el arte del aseo masculino a través de productos de
+          primera calidad <br /> totalmente naturales y utilitarios.{" "}
+        </h4>
+        {/* linea  */}
+        <div className="espacio">
+          <div className="titulo-promo">
+            <h2 className="titulo-promo2">
+              <GiMustache style={{ fontSize: "2.5em", marginBottom: "20px" }} />
+            </h2>
+          </div>
+        </div>
+      </div>
+      {/* producto */}
+      <div className="contieneCartas">
+        <div id="wrapper">
+          <div class="dust">
+            <img src="http://www.mattdrew.co.uk/experiments/images/ex-2point5d/dust.png" />
+          </div>
+          <div class="background">
+            <img src={imagenprueba} />
+          </div>
+        </div>
+        <div className="contenido">
+          <h1 className="titulo"> hola </h1>
+          <h4 className="subtitulo">subtitulo </h4>
+          <div class="control">
+            {/* <!-- Start Button buying --> */}
+            <button class="btn">
+              <span class="buy">Buy Now</span>
+            </button>
+            {/* <!-- End Button buying --> */}
+          </div>
+        </div>
+      </div>
 
- 
-</div>
-
-
-{/* calendario */}
-{/* <Ayudar/> */}
-{/* final calendario */}
-
-<div class="slider">
+      {/* <div class="slider"> carousel  comentado 
     <div class="slide-track">
         <div class="slide"></div>
         <div class="slide"></div>
@@ -63,9 +71,9 @@ function Mostrarcards () {
     </div>
 </div>
 
-    
-    </>
-  )
+     */}
+    </div>
+  );
 }
 
 export default Mostrarcards;
