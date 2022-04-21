7afa2ed4b652bd1c507578723fd4a99fac6d586c
kairayary <kaira.abreu@gmail.com>
2022-04-21 16:09:23 -0300

    Estilo Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,11 +13,11 @@ function Navbar() {
                 
                 <summary></summary>
                 <nav class="menu2">
-                <LinkRouter to='/'>Home</LinkRouter>
+                <LinkRouter to='/'>Inicio</LinkRouter>
                 <LinkRouter to='/productos'>Productos</LinkRouter>
                 <LinkRouter to='/servicios'>Servicios</LinkRouter>
                 <LinkRouter to='/signup'>Registro</LinkRouter>
-                <LinkRouter to='/signin'>SignIn</LinkRouter>
+                <LinkRouter to='/signin'>Ingresar</LinkRouter>
                 <LinkRouter to='/galeria'>Galeria</LinkRouter>
                 <LinkRouter to='/carrito'>Carrito</LinkRouter>
                 
